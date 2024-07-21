7a6feee7d84e14a7e27cfbb34be7cd0c3bd77b8c
Udhaykrishn <krishnauday320@gmail.com>
2024-07-21 17:26:17 +0530

    added all classes in app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './presentation/controllers/user.controller';
+import { BookController } from './presentation/controllers/book.controller';
+import { PrismaService } from './infrastruacure/prisma/prisma.service';
+import { PrismaUserRepository } from './infrastruacure/repositores/prisma-user.repository';
+import { PrismaBookRepository } from './infrastruacure/repositores/prisma-book.repository';
+import { CreateUserUseCase } from './application/use-cases/user/create-user.use-case';
+import { CreateBookUseCase } from './application/use-cases/book/create-book.use-case';
 
 
 @Module({
-  imports: [],
+  controllers:[UserController,BookController],
+  providers:[
+    PrismaService,
+    {
+      provide:"UserRepository",
+      useClass:PrismaUserRepository
+    },
+    {
+      provide:"BookRepository",
+      useClass:PrismaBookRepository
+    },
+    CreateUserUseCase,
+    CreateBookUseCase
+  ]
  
 })
 export class AppModule {}
